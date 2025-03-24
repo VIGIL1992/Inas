6d11730446b9467fea8394f7cd87bfed76a6f25f
VIGIL NATH K K <104504981+VIGIL1992@users.noreply.github.com>
2025-03-24 17:19:19 +0530

    Add files via upload

---
+++ hero.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	
+
+	<!-- Page Title -->
+    <title>Inas</title>
+	<!-- Favicon Icon -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/INAS_Final_LOGO.png">
+	<!-- Google Fonts Css-->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fustat:wght@200..800&amp;display=swap" rel="stylesheet">
+	<!-- Bootstrap Css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- SlickNav Css -->
+	<link href="css/slicknav.min.css" rel="stylesheet">
+	<!-- Swiper Css -->
+	<link rel="stylesheet" href="css/swiper-bundle.min.css">
+	<!-- Font Awesome Icon Css-->
+	<link href="css/all.css" rel="stylesheet" media="screen">
+	<!-- Animated Css -->
+	<link href="css/animate.css" rel="stylesheet">
+    <!-- Magnific Popup Core Css File -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Mouse Cursor Css File -->
+	<link rel="stylesheet" href="css/mousecursor.css">
+	<!-- Main Custom Css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+   
+
+    <!-- Header Start -->
+	<header class="main-header">
+		<div class="header-sticky">
+			<nav class="navbar navbar-expand-lg">
+				<div class="container-fluid">
+					<!-- Logo Start -->
+					<a class="navbar-brand" href="#index">
+						<!-- <img src="images/logo.svg" alt="Logo"> -->
+                        <img src="images/INAS_Final_LOGO.png" alt="Logo" style="height: 130px;">
+					</a>
+					<!-- Logo End -->
+
+					<!-- Main Menu Start -->
+					<div class="collapse navbar-collapse main-menu">
+                        <div class="nav-menu-wrapper">
+                            <ul class="navbar-nav mr-auto" id="menu">
+                                <li class="nav-item"><a class="nav-link" href="#home">Home</a> </li>                                
+                                <li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#service">Services</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>                             
+                            </ul>
+                        </div>
+                        <!-- Header Btn Start -->
+                        <!-- <div class="header-btn d-inline-flex">
+                            <a href="tel:+789859664" class="btn-phone"><i class="fa-solid fa-phone-volume"></i>+971 123 456 789</a>
+                            <a href="#contact" class="btn-default">get in touch</a>
+                        </div> -->
+                        <!-- Header Btn End -->
+					</div>
+					<!-- Main Menu End -->
+					<div class="navbar-toggle"></div>
+				</div>
+			</nav>
+			<div class="responsive-menu"></div>
+		</div>
+	</header>
+	<!-- Header End -->
+
+    <!-- Hero Section Start -->
+    <div class="hero hero1 bg-section parallaxie" id="home" style="background-position: center 1px !important;">
+        <div class="hero-section" >
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6 col-md-9">
+                        <!-- Hero Content Start -->
+                        <div class="hero-content">
+                            <!-- Section Title Start -->
+                            <div class="section-title">
+                                <h3 class="wow fadeInUp">Welcome To INAS.</h3>
+                                <h1 class="wow fadeInUp" data-wow-delay="0.2s">Inas <span>Life Healing</span></h1>
+                            </div>
+                            <!-- Section Title End -->
+    
+                            <!-- Hero Button Start -->
+                            <div class="hero-btn wow fadeInUp" data-wow-delay="0.4s">
+                                <a href="#contact" class="btn-default">get in touch</a>
+                            </div>
+                            <!-- Hero Button End -->
+                            <style>
+                                @media (max-width: 768px) {
+                                    .hero-btn {
+                                        text-align: center !important; /* Centers the button text on mobile */
+                                    }
+                                }
+                            </style>
+                        </div>
+                        <!-- Hero Content End -->
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="export-coaching-box wow fadeInUp" data-wow-delay="0.6s">
+                            <!-- Export Coaching Step Start -->
+                            <div class="export-coaching-step">
+                                <div class="icon-box">
+                                    <img src="images/icon-export-coaching-1.svg" alt="">
+                                </div>
+                                <div class="export-coaching-step-content">
+                                    <p>Transform Your Life with Natural Healing.</p>
+                                </div>
+                            </div>
+                            <!-- Export Coaching Step End -->
+
+                            <!-- Export Coaching Step Start -->
+                            <div class="export-coaching-step">
+                                <div class="icon-box">
+                                    <img src="images/icon-export-coaching-2.svg" alt="">
+                                </div>
+                                <div class="export-coaching-step-content">
+                                    <p>Find Balance & Inner Peace.</p>
+                                </div>
+                            </div>
+                            <!-- Export Coaching Step End -->
+
+                            <!-- Export Coaching Step Start -->
+                            <div class="export-coaching-step">
+                                <div class="icon-box">
+                                    <img src="images/icon-export-coaching-3.svg" alt="">
+                                </div>
+                                <div class="export-coaching-step-content">
+                                    <p>Experience the Power of Holistic Wellness.</p>
+                                </div>
+                            </div>
+                            <!-- Export Coaching Step End -->
+
+                            <!-- Export Coaching Step Start -->
+                            <div class="export-coaching-step">
+                                <div class="icon-box">
+                                    <img src="images/icon-export-coaching-4.svg" alt="">
+                                </div>
+                                <div class="export-coaching-step-content">
+                                    <p>Flexible Healing, Tailored for You.</p>
+                                </div>
+                            </div>
+                            <!-- Export Coaching Step End -->
+
+                            <!-- Export Coaching Step Start -->
+                            <div class="export-coaching-step">
+                                <div class="icon-box">
+                                    <img src="images/icon-export-coaching-5.svg" alt="">
+                                </div>
+                                <div class="export-coaching-step-content">
+                                    <p>Expert Guidance with Ongoing Support.</p>
+                                </div>
+                            </div>
+                            <!-- Export Coaching Step End -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero Section End -->
+
+
+
+
+
+
+    <!-- Jquery Library File -->
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <!-- Bootstrap js file -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Validator js file -->
+    <script src="js/validator.min.js"></script>
+    <!-- SlickNav js file -->
+    <script src="js/jquery.slicknav.js"></script>
+    <!-- Swiper js file -->
+    <script src="js/swiper-bundle.min.js"></script>
+    <!-- Counter js file -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <!-- Magnific js file -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- SmoothScroll -->
+    <script src="js/SmoothScroll.js"></script>
+    <!-- Parallax js -->
+    <script src="js/parallaxie.js"></script>
+    <!-- MagicCursor js file -->
+    <script src="js/gsap.min.js"></script>
+    <script src="js/magiccursor.js"></script>
+    <!-- Text Effect js file -->
+    <script src="js/SplitText.js"></script>
+    <script src="js/ScrollTrigger.min.js"></script>
+    <!-- YTPlayer js File -->
+    <script src="js/jquery.mb.YTPlayer.min.js"></script>
+    <!-- Wow js file -->
+    <script src="js/wow.js"></script>
+    <!-- Main Custom js file -->
+    <script src="js/function.js"></script>
+	<script src="../../assets/js/theme-panel.js"></script>
+</body>
+
+</html>
